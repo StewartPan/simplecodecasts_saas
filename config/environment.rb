b3393962fe9462577f7815bb217ccc671285686b
ZhiweiPan <panzw1105@gmail.com>
2017-05-15 03:56:27 +0000

    Fix typo about user_name and password

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,11 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :port           => '587',
   :address        => 'smtp.sendgrid.net',
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'heroku.com',
+  :port           => '587',
   :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
   :enable_startstls_auto => true
 }
-
-ActionMailer::Base.delivery_method = :smtp
